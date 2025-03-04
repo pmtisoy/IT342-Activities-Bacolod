02c0915aa7a754d234ab5bb703d04e0cc7557870
pmtisoy <pmtisoy@gmail.com>
2025-03-05 05:03:30 +0800

    viewcontacts

--- src/main/java/com/bacolod/google/oauth/controller/UserController.java
+++ src/main/java/com/bacolod/google/oauth/controller/UserController.java
@@ -1,31 +1,21 @@
 package com.bacolod.google.oauth.controller;
 
-import com.bacolod.google.oauth.service.peopleService;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.services.people.v1.PeopleService;
-import com.google.api.services.people.v1.model.ListConnectionsResponse;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+
+import com.bacolod.google.oauth.service.peopleService;
 import com.google.api.services.people.v1.model.Person;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import java.util.stream.Collectors;
 
 @Controller
 public class UserController {
@@ -47,9 +37,11 @@ public class UserController {
     }
     @GetMapping("/contacts")
     public ModelAndView getContacts(OAuth2AuthenticationToken authentication) throws IOException, GeneralSecurityException {
-        List<String> contacts = peopleService.getContacts(authentication);
+        List<Person> contacts = peopleService.getContacts(authentication);
         ModelAndView modelAndView = new ModelAndView("contacts"); // Name of your template
         modelAndView.addObject("contacts", contacts); // Add data to the model
         return modelAndView;
     }
+
+    
 }
--- src/main/java/com/bacolod/google/oauth/service/peopleService.java
+++ src/main/java/com/bacolod/google/oauth/service/peopleService.java
@@ -1,5 +1,16 @@
 package com.bacolod.google.oauth.service;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.stereotype.Service;
+
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
@@ -8,16 +19,6 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.people.v1.PeopleService;
 import com.google.api.services.people.v1.model.ListConnectionsResponse;
 import com.google.api.services.people.v1.model.Person;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.List;
 
 @Service
 public class peopleService {
@@ -53,10 +54,8 @@ public class peopleService {
 
         if (connections == null || connections.isEmpty()) {
             return List.of(); // Return an empty list
-        }
-        else{
-
-        return connections; // Return the list of Person objects
+        } else {
+            return connections;
         }
     }
 }
